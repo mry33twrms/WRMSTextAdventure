8a7bdd0c03a8bcc28192a7eea606d19a5c84c110
draffurd <deanyeats@gmail.com>
2026-06-03 11:44:39 -0400

    Added ED's Mobs

--- mobs.py
+++ mobs.py
@@ -19,3 +19,13 @@ class mob:
 # parabola = mob("Parabola", 100, 12)
 
 
+mob_dict = {
+    "Goblin" : {"hp": 10, "attack": 4, "weakness": "Fire"},
+    "Hydrogen Elemental" : {"hp": 82, "attack": 15, "weakness": "Oxygen"},
+    "Parabola" : {"hp": 100, "attack": 20, "weakness": "Calculator"},
+    "Insane Doctor" : {"hp": 35, "attack": 8, "weakness": "Therapy"},
+    "Possessed Canes" : {"hp": 20, "attack": 6,},
+    "Weaners & Beans" : {"hp": 15, "attack": 5,},
+    "Beholder": {"hp": 125, "attack": 25, "weakness": "Damaging the Eye"},
+    "Orc Guard": {"hp": 10, "attack": 5, "weakness": "Very Stupid"}
+}
